814ab427dc45c4b0f00c399a3ae636ceb59aade6
WajeehHussein <wajehessam3@gmail.com>
2022-04-21 10:49:10 +0300

    edit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 'use strict';
-
+//arrays
 let allEmployees = [];
 let department = ["Administration", "Marketing", "Development", "Finance"]
 let level = ["Junior", "MidSenior", "Senior"]
 
-function Employee(EmployeeID, FullName, department, level, ImageURL, Salary) {
 
+//constructor
+function Employee(EmployeeID, FullName, department, level, ImageURL, Salary) {
     this.EmployeeID = EmployeeID;
     this.FullName = FullName;
     this.Department = department;
@@ -16,7 +17,26 @@ function Employee(EmployeeID, FullName, department, level, ImageURL, Salary) {
 
 }
 
+Employee.prototype.render = function () {
+    document.write(`<h3>My name is ${this.FullName} and my salary is ${this.Salary} </h3>`)
+}
 
+//function
+Employee.prototype.salary = function () {
+    for (let i = 0; i < allEmployees.length; i++) {
+        if (this.Level === 'Junior') {
+            let a = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+            this.Salary = a(500, 1000)
+        } else if (this.Level === level[1]) {
+            let a = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+            this.Salary = a(1000, 1500)
+        } else if (this.Level === level[2]) {
+            let a = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+            this.Salary = a(1500, 2000)
+        }
+    }
+    return this.Salary = this.Salary * (1 - .075)
+}
 
 
 let ghazi = new Employee(1000, "Ghazi Samer", department[0], level[2]);
@@ -28,45 +48,10 @@ let rana = new Employee(1005, "Rana Saleh", department[2], level[0]);
 let hadi = new Employee(1006, "Hadi Ahmad", department[3], level[1]);
 
 
-
-
-Employee.prototype.salary = function () {
-    for (let i = 0; i < allEmployees.length; i++) {
-        if (this.Level === 'Junior') {
-            let a = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-            this.Salary = a(500, 1000)
-        } else if (this.Level === level[1]) {
-            let a = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-            this.Salary = a(1000, 1500)
-        } else if (this.Level === level[2]) {
-            let a = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-            this.Salary = a(1500, 2000)
-        }
-    }
-    return this.Salary = this.Salary - this.Salary * .075
+for (let i = 0; i < allEmployees.length; i++) {
+    allEmployees[i].salary();
+    allEmployees[i].render();
 }
-
-ghazi.salary()
-lana.salary()
-tamara.salary()
-safi.salary()
-omar.salary()
-rana.salary()
-hadi.salary()
-
-
-Employee.prototype.render = function () {
-    document.write(`My name is ${this.FullName} and my salary is ${this.Salary} \n`)
-}
-ghazi.render()
-lana.render()
-tamara.render()
-safi.render()
-omar.render()
-rana.render()
-hadi.render()
-
-
 
 
 
